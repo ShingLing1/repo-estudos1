b4947b6a46c90397d8edc8fef5863e9292746270
ShingLing1 <ShingLing1@users.noreply.github.com>
2025-09-03 20:52:34 -0300

    Add devcontainer setup

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -e
-echo "🔹 Instalando dependências extras..."
-apt-get update && apt-get install -y \
-  git curl wget unzip nano vim htop net-tools \
-  docker-compose tightvncserver xfce4 xfce4-goodies \
-  python3-pip
-pip3 install --upgrade pip
-pip3 install flask requests
-docker run -d -p 9000:9000 --name portainer \
-  --restart=always -v /var/run/docker.sock:/var/run/docker.sock \
-  -v portainer_data:/data portainer/portainer-ce
+apt-get update
+apt-get install -y python3 python3-pip nodejs npm docker.io docker-compose mongodb-clients tigervnc-standalone-server tigervnc-common
+pip3 install flask fastapi
+npm install -g express
